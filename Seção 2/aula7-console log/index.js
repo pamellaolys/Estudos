e17ada6331cae9c9120c0f7cd065f4622717def9
Pamella Santos <pamellaolys@gmail.com>
2021-10-06 14:00:35 -0300

    Aula console.log curso javaScript e TypeScript

---
+++ Seção 2/aula7-console log/index.js
@@ -0,0 +1,6 @@
+console.log("'Pamella Santos'"); /*quando eu quiser usar aspas simples no meu texto eu tenho que abrir e fechar o meu texto com aspas duplas*/ 
+console.log('"Pamella" Santos');/*quando eu quiser usar aspas duplas no meu texto eu tenho que abrir e fechar o meu texto com aspas simples*/ 
+console.log(`'Pamella' "Santos"`)/* crase possibilita  usar aspas duplas e/ou aspas simples no meu texto */ 
+/*Texto dentro de aspas (dupla e simples) e crase sao considerados String*/ 
+
+console.log(35, 15.85 , 'Pamella Santos'); /* em javaScript é quando for numeral é considerado Number = Número */
